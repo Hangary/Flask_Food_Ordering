ed340df40b03e6327f989844ef0263aea910f951
Hangar <HangaryLiu@gmail.com>
2019-04-09 17:29:46 +1000

    created a flask framework for front-end

---
+++ init.py
@@ -0,0 +1,15 @@
+from src.order_system import OrderSystem
+from src.system_creator import *
+
+
+def bootstrap_system():
+
+    system = create_save_system(
+        mains=create_mains_menu("docs/Menus.csv"),
+        sides=create_sides_menu("docs/Menus.csv"),
+        drinks=create_drinks_menu("docs/Menus.csv"),
+        inventory=create_inventory("docs/Inventory.csv"),
+        staff_system=create_staffsystem("docs/StaffSystem.csv")
+    )
+
+    return system
---
+++ routes.py
@@ -0,0 +1,99 @@
+from flask import render_template, request, redirect, url_for, abort
+from server import app, system
+from datetime import datetime
+
+
+
+
+'''
+Dedicated page for "page not found"
+'''
+@app.route('/404')
+@app.errorhandler(404)
+def page_not_found(e=None):
+    return render_template('404.html'), 404
+
+
+
+
+
+'''
+---- Supplied codes ----:
+'''
+
+
+'''
+Search for Cars
+'''
+@app.route('/', methods=["GET", "POST"])
+def cars():
+
+    if request.method == 'POST':
+        make  = request.form.get('make')
+        model = request.form.get('model')
+
+        if make == '':
+            make = None
+
+        if model == '':
+            model = None
+
+        cars = system.search_car(make, model)
+        return render_template('cars.html', cars = cars)
+    
+    return render_template('cars.html', cars = system.cars)
+
+
+
+
+'''
+Display car details for the car with given rego
+'''
+@app.route('/cars/<rego>')
+def car(rego):
+    car = system.get_car(rego)
+
+    if not car:
+        abort(404)
+
+    return render_template('car_details.html', car=car)
+
+
+'''
+Make a booking for a car with given rego
+'''
+from src.forms import BookingForm
+@app.route('/cars/book/<rego>', methods=["GET", "POST"])
+def book(rego):
+    car = system.get_car(rego)
+
+    if not car:
+        abort(404)
+    if request.method == 'POST':
+        form = BookingForm(request.form)
+        # 1. If form is not valid, then display appropriate error messages
+        if not form.is_valid:
+            errors = {field.name: form.get_error(field.name) for field in form._fields}
+            errors["period"] = form.get_error("period")
+            return render_template('booking_form.html', car=car,errors=errors,checked=False,form=form) 
+            # 2. If the user has pressed the 'check' button, then display the fee
+        
+        if request.form['button'] == "check_booking":
+            fee = system.check_fee(car, form.start_date, form.end_date)
+            return render_template('booking_form.html', car=car,errors={},checked=True,fee=fee,form=form)
+        # 3. Otherwise, if the user has pressed the 'confirm' button, then 
+        #   make the booking and display the confirmation page
+        elif request.form['button'] == "confirm":
+            customer = Customer(form.customer_name, form.customer_licence)
+            booking = system.make_booking(customer, car, form.start_date, form.end_date, form.start_location, form.end_location)
+            return render_template('booking_confirm.html', booking=booking)
+        elif request.form['button'] == "cancel":
+            pass
+    return render_template('booking_form.html', car=car,errors={},checked=False,form=None)
+
+'''
+Display list of all bookings for the car with given 'rego'
+'''
+@app.route('/cars/bookings/<rego>')
+def car_bookings(rego):
+    return render_template('bookings.html', bookings=system.get_bookings_by_car_rego(rego))
